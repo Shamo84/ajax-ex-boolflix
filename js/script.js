2d81dd17a16c0dc92c195ab142333e93ffbf2d0a
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-02-11 14:55:49 +0100

    4a milestone

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
+  var moviesUrl = "https://api.themoviedb.org/3/search/movie";
+  var seriesUrl = "https://api.themoviedb.org/3/search/tv";
+  var movieContainer = $(".movie.container");
+  var seriesContainer = $(".tvseries.container");
+
   $(document).on("click", "#search", function() {
     var ricerca = $("#input").val();
-    sendRequestToServer(ricerca);
+    sendRequestToServer(ricerca, moviesUrl, movieContainer);
+    sendRequestToServer(ricerca, seriesUrl, seriesContainer);
+    $("#input").val("");
+
   });
   $("#input").keyup(function(event) {
     if (event.which == 13) {
       var ricerca = $("#input").val();
-      sendRequestToServer(ricerca);
+      sendRequestToServer(ricerca, moviesUrl, movieContainer);
+      sendRequestToServer(ricerca, seriesUrl, seriesContainer);
+      $("#input").val("");
     }
+  });
+  $("#input").on("focus", function() {
+    $(this).attr("placeholder", "");
+  });
+  $("#input").on("blur", function() {
+    $(this).attr("placeholder", "Inserisci un titolo");
   });
 });
 
-function sendRequestToServer(ricerca) {
+function sendRequestToServer(ricerca, url, container) {
   if ($("#input").val() != "") {
-    $("#input").val("");
-    $(".movie.container").html("");
+    container.html("");
     $("#cercato > span").text(ricerca);
     $("#cercato").removeClass("hidden");
     $.ajax({
-      url: "https://api.themoviedb.org/3/search/movie",
+      url: url,
       method: "GET",
       data: {
         api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
@@ -29,30 +44,9 @@ function sendRequestToServer(ricerca) {
         if (risposta.total_results > 0) {
           $("h2.movie-title").removeClass("hidden");
           $("h2.tvseries-title").removeClass("hidden");
-          stampaFilm(risposta.results);
+          stampa(risposta.results, container);
         } else {
-          $(".movie.container").append("La ricerca non ha prodotto alcun risultato!");
-        }
-      },
-      error: function functionName() {
-      }
-    })
-    $.ajax({
-      url: "https://api.themoviedb.org/3/search/tv",
-      method: "GET",
-      data: {
-        api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
-        query: ricerca,
-        language: "it-IT"
-      },
-      success: function(risposta) {
-        $(".tvseries.container").html("");
-        if (risposta.total_results > 0) {
-          $("h2.movie-title").removeClass("hidden");
-          $("h2.tvseries-title").removeClass("hidden");
-          stampaSerie(risposta.results);
-        } else {
-          $(".tvseries.container").append("La ricerca non ha prodotto alcun risultato!");
+          container.append("La ricerca non ha prodotto alcun risultato!");
         }
       },
       error: function functionName() {
@@ -61,38 +55,27 @@ function sendRequestToServer(ricerca) {
   }
 }
 
-function stampaFilm(listaOggetti) {
+function stampa(listaOggetti, container) {
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
   for (var key in listaOggetti) {
     var context = {
       poster: listaOggetti[key].poster_path,
       title: listaOggetti[key].title,
-      releaseYear: moment(listaOggetti[key].release_date, "YYYY-MM-DD").format("YYYY"),
+      name: listaOggetti[key].name,
       originalTitle: listaOggetti[key].original_title,
+      originalName: listaOggetti[key].original_name,
       language: listaOggetti[key].original_language,
-      rating: listaOggetti[key].vote_average,
-      stars: printStars(listaOggetti[key].vote_average)
+      stars: printStars(listaOggetti[key].vote_average),
+      trama: listaOggetti[key].overview
     };
+    if (context.name == undefined) {
+      context.releaseYear = moment(listaOggetti[key].release_date, "YYYY-MM-DD").format("YYYY");
+    } else {
+      context.releaseYear = moment(listaOggetti[key].first_air_date, "YYYY-MM-DD").format("YYYY");
+    }
     var html = template(context);
-    $(".movie.container").append(html);
-  }
-}
-function stampaSerie(listaOggetti) {
-  var source = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-  for (var key in listaOggetti) {
-    var context = {
-      poster: listaOggetti[key].poster_path,
-      title: listaOggetti[key].name,
-      releaseYear: moment(listaOggetti[key].first_air_date, "YYYY-MM-DD").format("YYYY"),
-      originalTitle: listaOggetti[key].original_name,
-      language: listaOggetti[key].original_language,
-      rating: listaOggetti[key].vote_average,
-      stars: printStars(listaOggetti[key].vote_average)
-    };
-    var html = template(context);
-    $(".tvseries.container").append(html);
+    container.append(html);
   }
 }
 
