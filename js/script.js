6c927bed364947227032e23b4653bf8e6cfd7e35
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-02-11 18:36:53 +0100

    5a milestone

--- js/script.js
+++ js/script.js
@@ -3,18 +3,19 @@ $(document).ready(function() {
   var seriesUrl = "https://api.themoviedb.org/3/search/tv";
   var movieContainer = $(".movie.container");
   var seriesContainer = $(".tvseries.container");
+  stampaGeneri();
 
   $(document).on("click", "#search", function() {
     var ricerca = $("#input").val();
-    sendRequestToServer(ricerca, moviesUrl, movieContainer);
-    sendRequestToServer(ricerca, seriesUrl, seriesContainer);
+    sendRequestToServer(ricerca, moviesUrl, movieContainer, "movie");
+    sendRequestToServer(ricerca, seriesUrl, seriesContainer, "tv");
     $("#input").val("");
   });
   $("#input").keyup(function(event) {
     if (event.which == 13) {
       var ricerca = $("#input").val();
-      sendRequestToServer(ricerca, moviesUrl, movieContainer);
-      sendRequestToServer(ricerca, seriesUrl, seriesContainer);
+      sendRequestToServer(ricerca, moviesUrl, movieContainer, "movie");
+      sendRequestToServer(ricerca, seriesUrl, seriesContainer, "tv");
       $("#input").val("");
     }
   });
@@ -26,9 +27,11 @@ $(document).ready(function() {
   });
 });
 
-function sendRequestToServer(ricerca, url, container) {
+function sendRequestToServer(ricerca, url, container, string) {
   if ($("#input").val() != "") {
     container.html("");
+    generiFilm = [];
+    generiTv = [];
     $("#cercato > span").text(ricerca);
     $("#cercato").removeClass("hidden");
     $.ajax({
@@ -43,7 +46,7 @@ function sendRequestToServer(ricerca, url, container) {
         if (risposta.total_results > 0) {
           $("h2.movie-title").removeClass("hidden");
           $("h2.tvseries-title").removeClass("hidden");
-          stampa(risposta.results, container);
+          stampa(risposta.results, container, string);
         } else {
           container.append("La ricerca non ha prodotto alcun risultato!");
         }
@@ -54,11 +57,12 @@ function sendRequestToServer(ricerca, url, container) {
   }
 }
 
-function stampa(listaOggetti, container) {
+function stampa(listaOggetti, container, string) {
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
   for (var key in listaOggetti) {
     var context = {
+      id: listaOggetti[key].id,
       poster: listaOggetti[key].poster_path,
       title: listaOggetti[key].title,
       name: listaOggetti[key].name,
@@ -67,15 +71,16 @@ function stampa(listaOggetti, container) {
       language: listaOggetti[key].original_language,
       stars: printStars(listaOggetti[key].vote_average),
       trama: listaOggetti[key].overview,
-      cast: getActors(listaOggetti[key].id)
     };
-    if (context.name == undefined) {
+    if (string == "movie") {
       context.releaseYear = moment(listaOggetti[key].release_date, "YYYY-MM-DD").format("YYYY");
     } else {
       context.releaseYear = moment(listaOggetti[key].first_air_date, "YYYY-MM-DD").format("YYYY");
     }
     var html = template(context);
     container.append(html);
+    getActors(listaOggetti[key].id, string);
+    getGenres(listaOggetti[key].id, string);
   }
 }
 
@@ -91,23 +96,74 @@ function printStars(voto) {
   return stars;
 }
 
-function getActors(id) {
+function getActors(id, string) {
   var listaAttori = "";
   $.ajax({
-    url: "https://api.themoviedb.org/3/movie/" + id + "/credits",
+    url: "https://api.themoviedb.org/3/"+ string + "/" + id + "/credits",
     method: "GET",
     data: {
       api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
     },
     success: function(risposta) {
       for (var i = 0; i < 5; i++) {
-        console.log(risposta.cast[i].name);
         listaAttori += risposta.cast[i].name + ", ";
       }
-      console.log(listaAttori);
+      listaAttori = listaAttori.slice(0, listaAttori.length-2);
+      $("#" + id).find(".cast").append(listaAttori);
     },
     error: function functionName() {
     }
   })
-  return listaAttori;
+}
+function getGenres(id, string) {
+  var generi = "";
+  $.ajax({
+    url: "https://api.themoviedb.org/3/"+ string + "/" + id,
+    method: "GET",
+    data: {
+      api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
+    },
+    success: function(risposta) {
+    for (var key in risposta.genres) {
+      generi += risposta.genres[key].name + ", ";
+      if (string == "movie") {
+        if (!generiFilm.includes(risposta.genres[key].name)) {
+          generiFilm.push(risposta.genres[key].name);
+        }
+      } else {
+        if (!generiTv.includes(risposta.genres[key].name)) {
+          generiTv.push(risposta.genres[key].name);
+        }
+      }
+    }
+    console.log(generiFilm);
+    console.log(generiTv);
+      generi = generi.slice(0, generi.length-2);
+      $("#" + id).find(".generi").append(generi);
+    },
+    error: function functionName() {
+    }
+  })
+}
+function stampaGeneri() {
+  // $.ajax({
+  //   url: url,
+  //   method: "GET",
+  //   data: {
+  //     api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
+  //     query: ricerca,
+  //     language: "it-IT"
+  //   },
+  //   success: function(risposta) {
+  //     if (risposta.total_results > 0) {
+  //       $("h2.movie-title").removeClass("hidden");
+  //       $("h2.tvseries-title").removeClass("hidden");
+  //       stampa(risposta.results, container, string);
+  //     } else {
+  //       container.append("La ricerca non ha prodotto alcun risultato!");
+  //     }
+  //   },
+  //   error: function functionName() {
+  //   }
+  // })
 }
