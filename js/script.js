0c8f271037cb345404580a4ca6ebaecf870b1122
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-02-11 19:20:46 +0100

    rambooo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   var seriesUrl = "https://api.themoviedb.org/3/search/tv";
   var movieContainer = $(".movie.container");
   var seriesContainer = $(".tvseries.container");
-  stampaGeneri();
 
   $(document).on("click", "#search", function() {
     var ricerca = $("#input").val();
@@ -25,13 +24,22 @@ $(document).ready(function() {
   $("#input").on("blur", function() {
     $(this).attr("placeholder", "Inserisci un titolo");
   });
+  $("#movies-select").on("change", function() {
+    filtraPerGenere(".movie.container", "#movies-select");
+  })
+  $("#tv-select").on("change", function() {
+    filtraPerGenere(".tvseries.container", "#tv-select");
+  })
 });
 
 function sendRequestToServer(ricerca, url, container, string) {
   if ($("#input").val() != "") {
     container.html("");
+    $("#movies-select").html('<option value="">All</option>');
+    $("#tv-select").html('<option value="">All</option>');
     generiFilm = [];
     generiTv = [];
+    $("select").removeClass("hidden");
     $("#cercato > span").text(ricerca);
     $("#cercato").removeClass("hidden");
     $.ajax({
@@ -129,15 +137,16 @@ function getGenres(id, string) {
       if (string == "movie") {
         if (!generiFilm.includes(risposta.genres[key].name)) {
           generiFilm.push(risposta.genres[key].name);
+          printGenresinSelect("movies-select", risposta.genres[key].name);
+
         }
       } else {
         if (!generiTv.includes(risposta.genres[key].name)) {
           generiTv.push(risposta.genres[key].name);
+          printGenresinSelect("tv-select", risposta.genres[key].name);
         }
       }
     }
-    console.log(generiFilm);
-    console.log(generiTv);
       generi = generi.slice(0, generi.length-2);
       $("#" + id).find(".generi").append(generi);
     },
@@ -145,25 +154,22 @@ function getGenres(id, string) {
     }
   })
 }
-function stampaGeneri() {
-  // $.ajax({
-  //   url: url,
-  //   method: "GET",
-  //   data: {
-  //     api_key: "b1d8c49e5a444b10f55f930d8f4ed091",
-  //     query: ricerca,
-  //     language: "it-IT"
-  //   },
-  //   success: function(risposta) {
-  //     if (risposta.total_results > 0) {
-  //       $("h2.movie-title").removeClass("hidden");
-  //       $("h2.tvseries-title").removeClass("hidden");
-  //       stampa(risposta.results, container, string);
-  //     } else {
-  //       container.append("La ricerca non ha prodotto alcun risultato!");
-  //     }
-  //   },
-  //   error: function functionName() {
-  //   }
-  // })
+function printGenresinSelect(string, name) {
+  var source = $("#entry-template-2").html();
+  var template = Handlebars.compile(source);
+  var context = {
+      genere: name
+  };
+  var html = template(context);
+  $("#" + string).append(html);
+}
+function filtraPerGenere(string, select) {
+  var choice = $(select).val();
+  $(string).children(".show").each(function() {
+    if ($(this).find(".generi").text().includes(choice)) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
 }
